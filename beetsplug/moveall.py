c67038d91bca79d9fd52ab316ad9150c1ba1a236
Dale Sedivec <dale@codefu.org>
2015-06-22 09:34:31 -0500

    Update moveall for newer Beets
    Looks like the querying interface changed.  Logging also changed--and
    I've still got a problem with non-ASCII paths, not sure what to do with
    that.

--- beetsplug/moveall.py
+++ beetsplug/moveall.py
@@ -40,10 +40,10 @@ def handle_cli_exit(lib, **_kwargs):
             print ("moves out of %s have multiple dests, will not moveall" %
                    (src_dir,))
             continue
-        remaining_items = lib.items(library.PathQuery(src_dir))
+        remaining_items = lib.items(library.PathQuery('path', src_dir))
         if next(iter(remaining_items), None):
             print "some Beets items left in %s, will not moveall" % (src_dir,)
-        else:
+        elif os.path.exists(src_dir):
             print "moving all leftovers in %s to %s" % (src_dir, dst_dir)
             for dirent in os.listdir(src_dir):
                 dirent_path = os.path.join(src_dir, dirent)
@@ -51,6 +51,7 @@ def handle_cli_exit(lib, **_kwargs):
                     shutil.move(dirent_path, dst_dir)
                 except shutil.Error, ex:
                     # Log it but move on.
-                    log.critical("failed to move %r to %r: %s", dirent_path,
+                    # XXX unicode problem here if path has non-ascii
+                    log.critical("failed to move {0} to {1}: {2}", dirent_path,
                                  dst_dir, ex)
             util.prune_dirs(src_dir)
